7893349847a2a64587f7e9ac6291d5e1d0669dd2
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-06-27 20:32:36 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/hugodiniz/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.hugodiniz.workshopmongo.services;
+
+import com.hugodiniz.workshopmongo.domain.User;
+import com.hugodiniz.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
