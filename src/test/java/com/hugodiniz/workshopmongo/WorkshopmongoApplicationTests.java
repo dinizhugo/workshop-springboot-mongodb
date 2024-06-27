3e426bc90b18c2ac2829f13b37fe371e120da186
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-06-27 12:17:09 -0300

    first commit

---
+++ src/test/java/com/hugodiniz/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugodiniz.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
