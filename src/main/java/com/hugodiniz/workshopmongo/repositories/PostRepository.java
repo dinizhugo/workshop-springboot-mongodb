70ded83b355d44c171fa182687bb7bf8e515606c
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-06-30 17:03:13 -0300

    Consulta simples com @Query

--- src/main/java/com/hugodiniz/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/hugodiniz/workshopmongo/repositories/PostRepository.java
@@ -2,11 +2,16 @@ package com.hugodiniz.workshopmongo.repositories;
 
 import com.hugodiniz.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': {$regex: ?0, $options: 'i'} }") //"?0" indica que vai receber o primeiro parametro do método
+    List<Post> searchTitle(String txt);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
